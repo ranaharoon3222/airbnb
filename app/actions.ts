e3e1cdafa915728c6005abd14348cb9414fc2958
ranaharoon3222 <ranaharoon3222@gmail.com>
2024-03-04 09:03:19 +0500

    fixed

--- app/actions.ts
+++ app/actions.ts
@@ -1,10 +1,10 @@
-"use server";
+'use server';
 
-import { redirect } from "next/navigation";
-import prisma from "./lib/db";
-import { supabase } from "./lib/supabase";
-import { revalidatePath } from "next/cache";
-import path from "path";
+import { redirect } from 'next/navigation';
+import prisma from './lib/db';
+import { supabase } from './lib/supabase';
+import { revalidatePath } from 'next/cache';
+import path from 'path';
 
 export async function createAirbnbHome({ userId }: { userId: string }) {
   const data = await prisma.home.findFirst({
@@ -12,7 +12,7 @@ export async function createAirbnbHome({ userId }: { userId: string }) {
       userId: userId,
     },
     orderBy: {
-      createdAT: "desc",
+      createdAT: 'desc',
     },
   });
 
@@ -54,8 +54,8 @@ export async function createAirbnbHome({ userId }: { userId: string }) {
 }
 
 export async function createCategoryPage(formData: FormData) {
-  const categoryName = formData.get("categoryName") as string;
-  const homeId = formData.get("homeId") as string;
+  const categoryName = formData.get('categoryName') as string;
+  const homeId = formData.get('homeId') as string;
   const data = await prisma.home.update({
     where: {
       id: homeId,
@@ -70,21 +70,21 @@ export async function createCategoryPage(formData: FormData) {
 }
 
 export async function CreateDescription(formData: FormData) {
-  const title = formData.get("title") as string;
-  const description = formData.get("description") as string;
-  const price = formData.get("price");
-  const imageFile = formData.get("image") as File;
-  const homeId = formData.get("homeId") as string;
+  const title = formData.get('title') as string;
+  const description = formData.get('description') as string;
+  const price = formData.get('price');
+  const imageFile = formData.get('image') as File;
+  const homeId = formData.get('homeId') as string;
 
-  const guestNumber = formData.get("guest") as string;
-  const roomNumber = formData.get("room") as string;
-  const bathroomsNumber = formData.get("bathroom") as string;
+  const guestNumber = formData.get('guest') as string;
+  const roomNumber = formData.get('room') as string;
+  const bathroomsNumber = formData.get('bathroom') as string;
 
   const { data: imageData } = await supabase.storage
-    .from("images")
+    .from('images')
     .upload(`${imageFile.name}-${new Date()}`, imageFile, {
-      cacheControl: "2592000",
-      contentType: "image/png",
+      cacheControl: '2592000',
+      contentType: 'image/png',
     });
 
   const data = await prisma.home.update({
@@ -107,8 +107,8 @@ export async function CreateDescription(formData: FormData) {
 }
 
 export async function createLocation(formData: FormData) {
-  const homeId = formData.get("homeId") as string;
-  const countryValue = formData.get("countryValue") as string;
+  const homeId = formData.get('homeId') as string;
+  const countryValue = formData.get('countryValue') as string;
   const data = await prisma.home.update({
     where: {
       id: homeId,
@@ -119,13 +119,13 @@ export async function createLocation(formData: FormData) {
     },
   });
 
-  return redirect("/");
+  return redirect('/');
 }
 
 export async function addToFavorite(formData: FormData) {
-  const homeId = formData.get("homeId") as string;
-  const userId = formData.get("userId") as string;
-  const pathName = formData.get("pathName") as string;
+  const homeId = formData.get('homeId') as string;
+  const userId = formData.get('userId') as string;
+  const pathName = formData.get('pathName') as string;
 
   const data = await prisma.favorite.create({
     data: {
@@ -138,9 +138,9 @@ export async function addToFavorite(formData: FormData) {
 }
 
 export async function DeleteFromFavorite(formData: FormData) {
-  const favoriteId = formData.get("favoriteId") as string;
-  const pathName = formData.get("pathName") as string;
-  const userId = formData.get("userId") as string;
+  const favoriteId = formData.get('favoriteId') as string;
+  const pathName = formData.get('pathName') as string;
+  const userId = formData.get('userId') as string;
 
   const data = await prisma.favorite.delete({
     where: {
@@ -153,10 +153,10 @@ export async function DeleteFromFavorite(formData: FormData) {
 }
 
 export async function createReservation(formData: FormData) {
-  const userId = formData.get("userId") as string;
-  const homeId = formData.get("homeId") as string;
-  const startDate = formData.get("startDate") as string;
-  const endDate = formData.get("endDate") as string;
+  const userId = formData.get('userId') as string;
+  const homeId = formData.get('homeId') as string;
+  const startDate = formData.get('startDate') as string;
+  const endDate = formData.get('endDate') as string;
 
   const data = await prisma.reservation.create({
     data: {
@@ -167,5 +167,5 @@ export async function createReservation(formData: FormData) {
     },
   });
 
-  return redirect("/");
+  return redirect('/');
 }
--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -1,7 +1,8 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import prisma from "@/app/lib/db";
-import { NextResponse } from "next/server";
-import { unstable_noStore as noStore } from "next/cache";
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import prisma from '@/app/lib/db';
+import { NextResponse } from 'next/server';
+import { unstable_noStore as noStore } from 'next/cache';
+import { env } from 'process';
 
 export async function GET() {
   noStore();
@@ -10,7 +11,7 @@ export async function GET() {
   const user = await getUser();
 
   if (!user || user === null || !user.id) {
-    throw new Error("Smoething went wrong, i am srorry....");
+    throw new Error('Smoething went wrong, i am srorry....');
   }
 
   let dbUser = await prisma.user.findUnique({
@@ -22,15 +23,16 @@ export async function GET() {
   if (!dbUser) {
     dbUser = await prisma.user.create({
       data: {
-        email: user.email ?? "",
-        firstName: user.given_name ?? "",
-        lastName: user.family_name ?? "",
+        email: user.email ?? '',
+        firstName: user.given_name ?? '',
+        lastName: user.family_name ?? '',
         id: user.id,
         profileImage:
           user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
       },
     });
   }
+  let url: any = process.env.KINDE_SITE_URL;
 
-  return NextResponse.redirect("https://airbnb-yt-peach.vercel.app");
+  return NextResponse.redirect(url);
 }
